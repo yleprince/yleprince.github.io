ed04de524634c15d9ca13a52f8c819c2956adfda
yleprince <yrieix.leprince@gmail.com>
2018-05-10 01:09:48 +0200

    adding games

---
+++ games/setup.js
@@ -0,0 +1,10 @@
+let content = [
+  {'id_': 'flappy ball',
+    'href_': '/games/flappy',
+    'class_': 'fab fa-earlybirds'},
+  {'id_': 'snake',
+    'href_': '/games/snake/',
+    'class_': 'fab fa-stripe-s'}
+];
+
+let page_title = 'games';

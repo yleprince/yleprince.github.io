e36dd353d1e72c2db0a37ffb3a567f8be8d9db62
yleprince <yrieix.leprince@gmail.com>
2018-05-10 00:58:24 +0200

    first test for new style

---
+++ setup.js
@@ -0,0 +1,13 @@
+let content = [
+  {'id_': 'algorithms',
+    'href_': '/algorithms',
+    'class_': 'fas fa-code-branch'},
+  {'id_': 'resources',
+    'href_': '/resources',
+    'class_': 'fas fa-database'},
+  {'id_': 'games',
+    'href_': '/games',
+    'class_': 'fas fa-gamepad'}
+];
+
+let page_title = 'homepage';

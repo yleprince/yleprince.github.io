c2b7333724772b74ad9274105f156e7af4902973
yleprince <yrieix.leprince@gmail.com>
2018-05-10 16:05:36 +0200

    resources new structure

---
+++ resources/setup.js
@@ -0,0 +1,16 @@
+let content = [
+  {'id_': 'python',
+    'href_': '/resources/python',
+    'class_': 'fab fa-python'},
+  {'id_': 'javascript',
+    'href_': '/resources/javascript',
+    'class_': 'fab fa-js-square'},
+  {'id_': 'use cases',
+    'href_': '/resources/use_cases',
+    'class_': 'fas fa-cogs'},
+  {'id_': 'setup tutorials',
+    'href_': '/resources/tutorials',
+    'class_': 'fab fa-linux'}
+];
+
+let page_title = 'resources';

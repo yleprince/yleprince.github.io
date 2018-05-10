e451ec7fc0e5b49d06221beb255e2f108fc98b13
yleprince <yrieix.leprince@gmail.com>
2018-05-10 16:04:55 +0200

    commit games setup.js

--- games/setup.js
+++ games/setup.js
@@ -1,10 +1,12 @@
 let content = [
   {'id_': 'flappy ball',
     'href_': '/games/flappy',
-    'class_': 'fab fa-earlybirds'},
+    'class_': 'fab fa-earlybirds',
+	'content':'F'},
   {'id_': 'snake',
     'href_': '/games/snake/',
-    'class_': 'fab fa-stripe-s'}
+    'class_': 'fab fa-stripe-s',
+	'content': 'S'}
 ];
 
 let page_title = 'games';

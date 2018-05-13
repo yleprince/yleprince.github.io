b9b954c6dcc0466179a87601eeb97b9b4bdf2fc1
yleprince <yrieix.leprince@gmail.com>
2018-05-13 10:30:22 +0200

    adding index page for algorithms section

---
+++ algorithms/setup.js
@@ -0,0 +1,13 @@
+let content = [
+  {'id_': 'kmeans',
+    'href_': '/algorithms/kmeans/',
+    'class_': 'fab fa-kickstarter-k'},
+  {'id_': 'Genetic Algorithm rockets',
+    'href_': '/algorithms/rockets/',
+    'class_': 'fas fa-rocket'},
+  {'id_': 'Steering behavior',
+    'href_': '/algorithms/steering/example/',
+    'class_': 'fas fa-paper-plane'}
+];
+
+let page_title = 'homepage';

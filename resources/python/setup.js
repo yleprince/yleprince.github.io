7a88fa7ae98ab3305279a810bfec58885471afe5
yleprince <yrieix.leprince@gmail.com>
2018-05-13 09:56:51 +0200

    adding python index page

---
+++ resources/python/setup.js
@@ -0,0 +1,10 @@
+let content = [
+  {'id_': 'line chart',
+    'href_': '/resources/python/linechart/',
+    'class_': 'fas fa-chart-line'},
+  {'id_': 'matshow',
+    'href_': '/resources/python/matshow/',
+    'class_': 'fas fa-square'}
+];
+
+let page_title = 'python';

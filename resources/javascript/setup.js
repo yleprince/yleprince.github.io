7ee33c73f5ea14110cd1eeb4358b5c3ab77f4d2f
yleprince <yrieix.leprince@gmail.com>
2018-05-13 10:18:11 +0200

    moving classics (clocks and stopwatch) into resources/javascript

--- resources/javascript/setup.js
+++ resources/javascript/setup.js
@@ -1,4 +1,13 @@
 let content = [
+  {'id_': 'bar clock',
+    'href_': '/resources/javascript/clock2/',
+    'class_': 'fas fa-align-center'},
+  {'id_': 'hexa clock',
+    'href_': '/resources/javascript/hexaclock/',
+    'class_': 'far fa-clock'},
+  {'id_': 'stopwatch',
+    'href_': '/resources/javascript/stopwatch/',
+    'class_': 'fas fa-stopwatch'},
   {'id_': 'france density map',
     'href_': '/resources/javascript/d3/',
     'class_': 'fas fa-map-pin'}

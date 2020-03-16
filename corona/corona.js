cc2385089e27fb3be12f3ac2e1051ca0af617045
yleprince <yrieix.leprince@gmail.com>
2020-03-16 21:41:46 +0100

    corona

---
+++ corona/corona.js
@@ -0,0 +1,22 @@
+console.log('corona');
+const url = "https://thevirustracker.com/free-api?global=stats";
+
+const d = new Date;
+
+let hours = d.getHours().toString();
+let minutes = d.getMinutes().toString();
+
+hours = hours.length < 2 ? '0' + hours : hours;
+minutes = minutes.length < 2 ? '0' + minutes : minutes;
+const date_str = hours + ":" + minutes;
+
+document.getElementById('source').textContent = date_str;
+
+const update = () => fetch('https://thevirustracker.com/free-api?global=stats').then(res => res.json()).then(data => {
+    Object.keys(data.results[0])
+        .filter(k => k != "source")
+        .map(id =>
+            document.getElementById(id).textContent = data.results[0][id]);
+});
+
+update();

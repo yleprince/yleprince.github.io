da9911cd0760e4700652b39d1a72f11620c15f93
yleprince <yrieix.leprince@gmail.com>
2020-03-16 21:47:28 +0100

    adding route to coronavirus

--- setup.js
+++ setup.js
@@ -1,13 +1,24 @@
 let content = [
-  {'id_': 'algorithms',
+  {
+    'id_': 'coronavirus',
+    'href_': '/corona',
+    'class_': 'fas fa-user-md'
+  },
+  {
+    'id_': 'algorithms',
     'href_': '/algorithms',
-    'class_': 'fas fa-code-branch'},
-  {'id_': 'resources',
+    'class_': 'fas fa-code-branch'
+  },
+  {
+    'id_': 'resources',
     'href_': '/resources',
-    'class_': 'fas fa-database'},
-  {'id_': 'games',
+    'class_': 'fas fa-database'
+  },
+  {
+    'id_': 'games',
     'href_': '/games',
-    'class_': 'fas fa-gamepad'}
+    'class_': 'fas fa-gamepad'
+  }
 ];
 
 let page_title = 'homepage';

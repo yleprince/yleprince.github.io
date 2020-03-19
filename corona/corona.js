82e6dc397f55ce17709d92127f1508b379e70c0b
yleprince <yrieix.leprince@gmail.com>
2020-03-19 18:59:25 +0100

    update corona js

--- corona/corona.js
+++ corona/corona.js
@@ -1,5 +1,4 @@
-console.log('corona');
-const url = "https://thevirustracker.com/free-api?global=stats";
+const url = 'https://thevirustracker.com/free-api?global=stats';
 
 const d = new Date;
 
@@ -8,15 +7,19 @@ let minutes = d.getMinutes().toString();
 
 hours = hours.length < 2 ? '0' + hours : hours;
 minutes = minutes.length < 2 ? '0' + minutes : minutes;
-const date_str = hours + ":" + minutes;
+const date_str = hours + ':' + minutes;
 
 document.getElementById('source').textContent = date_str;
 
-const update = () => fetch('https://thevirustracker.com/free-api?global=stats').then(res => res.json()).then(data => {
-    Object.keys(data.results[0])
-        .filter(k => k != "source")
-        .map(id =>
-            document.getElementById(id).textContent = data.results[0][id]);
-});
+const update = () => fetch('https://thevirustracker.com/free-api?global=stats')
+    .then(res => res.json())
+    .then(data => {
+        console.log('data', data)
+        Object.keys(data.results[0])
+            .filter(k => k != 'source')
+            .map(id =>
+                document.getElementById(id).textContent = data.results[0][id]);
+    })
+    .catch(console.error);
 
 update();

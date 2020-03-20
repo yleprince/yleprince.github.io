f7460910627bf71e2aea7900521a19011a88818e
yleprince <yrieix.leprince@gmail.com>
2020-03-20 22:39:47 +0100

    update evolution

--- corona-evolution/corona-evolution.js
+++ corona-evolution/corona-evolution.js
@@ -1,5 +1,6 @@
-const colors = ['#601A4A', '#EE442F', '#63ACBE', '#49494D']
-const countries = ['US', 'FR', 'IT'];
+const colors = ['#63ACBE', '#EE442F', '#601A4A', '#49494D'];
+
+const countryCheckboxes = document.querySelectorAll('#selectedCountries input');
 
 // set the dimensions and margins of the graph
 let margin = { top: 10, right: 30, bottom: 30, left: 60 },
@@ -25,68 +26,88 @@ const getData = async (country) => fetch(url(country))
         .map(([date, rest]) => ({ date: d3.timeParse("%m/%d/%Y")(date), ...rest }))
     );
 
-Promise.all(countries.map(getData))
-    .then(full => {
-        // X axis
-        let x = d3.scaleTime()
-            .domain(d3.extent(full.flat(), (d) => d.date))
-            .range([0, width]);
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .attr("class", "axis x")
-            .call(d3.axisBottom(x));
+const update = () => {
+    const countries = Array.from(countryCheckboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(({ value }) => value);
 
-        // Y axis
-        const max_y = d3.max(full.map(series => d3.max(series.map(d => d[selected]))));
-        let y = d3.scaleLinear()
-            .domain([1, max_y])
-            .range([height, 0]);
-        let yAxis = d3.axisLeft()
-            .scale(y)
-            .tickFormat(d3.format(","))
-            .ticks(4);
+    const tmp_colors = [...colors].reverse();
+    Array.from(countryCheckboxes)
+        .forEach((c, i) => {
+            c.labels[0].style.color = c.checked ? tmp_colors.pop() : '#444';
+        });
 
-        svg.append("g")
-            .attr("class", "axis y")
-            .call(yAxis);
+    console.log(countries);
 
-        full.map((data, i) => svg.append("path")
-            .attr("class", "line")
-            .datum(data.filter(d => d[selected] > 0))
-            .attr("fill", "none")
-            .attr("stroke", colors[i])
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                .x((d) => x(d.date))
-                .y((d) => y(d[selected]))
-            ));
+    Promise.all(countries.map(getData))
+        .then(full => {
+            console.log(full);
+            svg.selectAll("*").remove();
+            // X axis
+            let x = d3.scaleTime()
+                .domain(d3.extent(full.flat(), (d) => d.date))
+                .range([0, width]);
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .attr("class", "axis x")
+                .call(d3.axisBottom(x));
 
-        d3.select("#logCheckbox").on("click", function () {
-            console.log('clicked');
-            if (this.checked) {
-                console.log('checked');
-                y = d3.scaleLog()
-                    .domain([1, max_y])
-                    .range([height, 0]);
-            } else {
-                console.log('unchecked');
-                y = d3.scaleLinear()
-                    .domain([1, max_y])
-                    .range([height, 0]);
-            }
-            yAxis.scale(y);
-            d3.select("g.axis.y")
-                .transition()
-                .duration(500)
+            // Y axis
+            const max_y = d3.max(full.map(series => d3.max(series.map(d => d[selected]))));
+            let y = d3.scaleLinear()
+                .domain([1, max_y])
+                .range([height, 0]);
+            let yAxis = d3.axisLeft()
+                .scale(y)
+                .tickFormat(d3.format(","))
+                .ticks(4);
+
+            svg.append("g")
+                .attr("class", "axis y")
                 .call(yAxis);
 
-            d3.selectAll(".line")
-                .transition()
-                .duration(500)
+
+            full.map((data, i) => svg.append("path")
+                .attr("class", "line")
+                .datum(data.filter(d => d[selected] > 0))
+                .attr("fill", "none")
+                .attr("stroke", colors[i])
+                .attr("stroke-width", 1.5)
                 .attr("d", d3.line()
                     .x((d) => x(d.date))
                     .y((d) => y(d[selected]))
-                )
-        })
-    });
+                ));
 
+            d3.select("#logCheckbox").on("click", function () {
+                console.log('clicked');
+                if (this.checked) {
+                    console.log('checked');
+                    y = d3.scaleLog()
+                        .domain([1, max_y])
+                        .range([height, 0]);
+                } else {
+                    console.log('unchecked');
+                    y = d3.scaleLinear()
+                        .domain([1, max_y])
+                        .range([height, 0]);
+                }
+                yAxis.scale(y);
+                d3.select("g.axis.y")
+                    .transition()
+                    .duration(500)
+                    .call(yAxis);
+
+                d3.selectAll(".line")
+                    .transition()
+                    .duration(500)
+                    .attr("d", d3.line()
+                        .x((d) => x(d.date))
+                        .y((d) => y(d[selected]))
+                    )
+            })
+        });
+
+}
+
+countryCheckboxes.forEach(d => d.addEventListener('input', update));
+update();

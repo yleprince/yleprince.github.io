b134f1dfc9f7fd2090f720630c88975c8cfa7d71
yleprince <yrieix.leprince@gmail.com>
2020-03-22 18:18:11 +0100

    update map

--- map/map.js
+++ map/map.js
@@ -1,12 +1,5 @@
-console.log('Hello map');
-
 const width = document.getElementById("container").offsetWidth * 0.95;
 const height = 500;
-const legendCellSize = 20;
-const colors = ['#d4eac7', '#c6e3b5', '#b7dda2', '#a9d68f',
-    '#9bcf7d', '#8cc86a', '#7ec157', '#77be4e', '#70ba45',
-    '#65a83e', '#599537', '#4e8230', '#437029', '#385d22',
-    '#2d4a1c', '#223815'];
 
 const svg = d3.select('#map').append("svg")
     .attr("id", "svg")
@@ -24,32 +17,46 @@ const path = d3.geoPath()
 
 const cGroup = svg.append("g");
 
-var promises = [];
-promises.push(d3.json("worldGeojson.json"));
-promises.push(fetch("https://thevirustracker.com/timeline/map-data.json").then(res => res.json()))
+d3.json("worldGeojson.json")
+    .then(geojson => {
+        const b = path.bounds(geojson),
+            s = 1 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+            t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
 
-Promise.all(promises).then((values) => {
-    const geojson = values[0];
-    const data = values[1];
-    console.log(data);
+        projection
+            .scale(s)
+            .translate(t);
 
-    var b = path.bounds(geojson),
-        s = .90 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
-        t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+        cGroup.selectAll("path")
+            .data(geojson.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("id", (d) => d.id)
+            .attr("name", (d) => d.name)
+            .attr("class", "country")
+            .on("click", function (d) {
+                d3.selectAll('.country').classed("selected", false)
+                d3.select(this).classed("selected", true);
+                document.getElementById("country").innerHTML = d.properties.name;
+                console.log(d);
+                updateCountryData(d.id);
+            });
 
-    projection
-        .scale(s)
-        .translate(t);
+        updateCountryData('FR');
+        d3.select('#FR').classed("selected", true);
+        document.getElementById("country").innerHTML = 'France';
+    });
 
-    cGroup.selectAll("path")
-        .data(geojson.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .attr("id", (d) => "code" + d.id)
-        .attr("class", "country");
+const countryStats = ['c_total_cases', 'c_total_recovered', 'c_total_unresolved',
+    'c_total_deaths', 'c_total_new_cases_today', 'c_total_new_deaths_today',
+    'c_total_active_cases', 'c_total_serious_cases'];
 
-    function shortCountryName(country) {
-        return country.replace("Démocratique", "Dem.").replace("République", "Rep.");
-    }
-});
+const urlCountry = (code) => `https://thevirustracker.com/free-api?countryTotal=${code}`;
+const updateCountryData = (code) => fetch(urlCountry(code))
+    .then(res => res.json())
+    .then(data => countryStats.forEach(k => {
+        const span = document.getElementById(k);
+        span.innerHTML = data.countrydata[0][k.slice(2)];
+    }));
+

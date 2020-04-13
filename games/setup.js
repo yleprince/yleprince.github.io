daee58b786597cd31d35b0ea70bf4f463db939b3
yleprince <yrieix.leprince@gmail.com>
2020-04-13 03:30:21 +0200

    adding link to website

--- games/setup.js
+++ games/setup.js
@@ -1,12 +1,22 @@
 let content = [
-  {'id_': 'flappy ball',
+  {
+    'id_': 'bingo',
+    'href_': '/games/bingo',
+    'class_': 'fas fa-edit',
+    'content': 'B'
+  },
+  {
+    'id_': 'flappy ball',
     'href_': '/games/flappy',
     'class_': 'fab fa-earlybirds',
-	'content':'F'},
-  {'id_': 'snake',
+    'content': 'F'
+  },
+  {
+    'id_': 'snake',
     'href_': '/games/snake/',
     'class_': 'fab fa-stripe-s',
-	'content': 'S'}
+    'content': 'S'
+  }
 ];
 
 let page_title = 'games';

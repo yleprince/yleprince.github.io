df1bde47b51f0088ca1f2adc6e0725075a4375ff
yleprince <yrieix.leprince@gmail.com>
2020-04-19 02:43:33 +0200

    adding polar plot

--- resources/python/setup.js
+++ resources/python/setup.js
@@ -1,16 +1,29 @@
 let content = [
-  {'id_': 'code tips',
+  {
+    'id_': 'code tips',
     'href_': '/resources/python/code/tips/',
-    'class_': 'fab fa-stack-overflow'},
-  {'id_': 'code setup',
+    'class_': 'fab fa-stack-overflow'
+  },
+  {
+    'id_': 'code setup',
     'href_': '/resources/python/code/setup/',
-    'class_': 'fas fa-plug'},
-  {'id_': 'line chart',
+    'class_': 'fas fa-plug'
+  },
+  {
+    'id_': 'line chart',
     'href_': '/resources/python/linechart/',
-    'class_': 'fas fa-chart-line'},
-  {'id_': 'matshow',
+    'class_': 'fas fa-chart-line'
+  },
+  {
+    'id_': 'matshow',
     'href_': '/resources/python/matshow/',
-    'class_': 'fab fa-delicious'}
+    'class_': 'fab fa-delicious'
+  },
+  {
+    'id_': 'polar chart',
+    'href_': '/resources/python/polar/',
+    'class_': 'fas fa - asterisk'
+  }
 ];
 
 let page_title = 'python';

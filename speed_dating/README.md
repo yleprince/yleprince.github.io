3ddbc624e5e29949b51b551ba2d1791efca84292
Yrieix Leprince <yrieix.leprince@gmail.com>
2021-01-22 20:33:33 +0100

    rm speed-dating + 404 page

--- speed_dating/README.md
+++
@@ -1,2 +0,0 @@
-# never-alone
-IGR204
--- speed_dating/js/modules/defaultOptions.js
+++
@@ -1,8 +0,0 @@
-export default function fillWithDefault(options, defaultOption) {
-    for (let attr in defaultOption) {
-        if (defaultOption.hasOwnProperty(attr) && !options.hasOwnProperty(attr)) {
-            options[attr] = defaultOption[attr];
-        }
-    }
-    return options;
-}
